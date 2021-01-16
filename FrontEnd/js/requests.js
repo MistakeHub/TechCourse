a14376d0eaed555625715f468460b2714cfa2dd7
VladSmorodin <vdsmorodin2003@gmail.com>
2021-01-16 19:27:10 +0300

    FrontEndCommit17

--- FrontEnd/js/requests.js
+++ FrontEnd/js/requests.js
@@ -1,4 +1,5 @@
 // запрос 1
+// 1.Фамилия, имя, отчество и адрес владельца автомобиля с данным номером государственной регистрации?
 function GetQuery1(data) {
     $("#GetQuery1").html(
         data.reduce(
@@ -14,11 +15,35 @@ function GetQuery1(data) {
     );
 }
 
-
 $(function () {
     $.ajax({
         url: "https://localhost:44354/api/Query/Query1/DT43DK",
         type: 'GET',
         success: (response) => GetQuery1(response)
+    })
+})
+
+// запросы 2
+// 2.Марка и год выпуска автомобиля данного владельца?
+function GetQuery2(data) {
+    $("#GetQuery2").html(
+        data.reduce(
+            (ans, elem) =>
+                ans +
+                `<tr>
+                <td>${elem.id}</td>
+                <td>${elem.mark}</td>
+                <td>${elem.yearOfIssueAuto}</td>
+               </tr>`,
+            ""
+        )
+    );
+}
+
+$(function () {
+    $.ajax({
+        url: "https://localhost:44354/api/Query/Query2/",
+        type: 'GET',
+        success: (response) => GetQuery2(response)
     })
 })
